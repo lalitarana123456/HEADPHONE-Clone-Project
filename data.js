7053c122f7d96f641abdcd24bc4656e86ff698d3
Ashish Rana <lalitakumari39840@gmail.com>
2022-05-07 17:45:11 +0530

    clone project

---
+++ data.js
@@ -0,0 +1,133 @@
+var prod=[
+    {
+        btn:"BEST SELLER",
+        img:"https://cdn.shopify.com/s/files/1/0153/8863/products/Headphone-Zone-KZ-ZSN-Pro-X-Black-03_800x.jpg?v=1623407931",
+        name:"KZ - ZSN PRO X",
+        productid:"er13",
+        type:"In-Ears With 1 DD+BA Dreivers",
+        price:"Rs 1,799",
+       
+    },
+
+
+    {
+        btn:"AWARD WINNER",
+        img:"https://cdn.shopify.com/s/files/1/0153/8863/products/Headphone-Zone-Final-Audio-E1000-BALCK-No-Mic-03_400x.jpg?v=1632819650",
+        name:"FINAL - E1000",
+        productid:"er12",
+        type:"In-Ears With 1 Dynamic Dreivers",
+        price:"Rs 1,799",
+      
+    },
+    
+    {
+        btn:"BEST SELLER",
+        img:"https://cdn.shopify.com/s/files/1/0153/8863/products/Headphone-Zone-CCA-CRA-Black-5_400x.jpg?v=1648213897",
+        name:"CCA - CRA",
+        productid:"er11",
+        type:"In-Ears With 1 Dynamic Dreivers",
+        price:"Rs 1,399",
+        
+    },
+
+    {
+        btn:"NEW",
+        img:"https://cdn.shopify.com/s/files/1/0153/8863/products/Headphone-Zone-FiiO-JD3-02_400x.jpg?v=1649502893",
+        name:"FIIO - JD3",
+        productid:"er1",
+        type:"In-Ears With 1 Dynamic Dreivers",
+        price:"Rs 1,499",
+       
+    },
+
+    {
+        btn:"BEST SELLER",
+        img:"https://cdn.shopify.com/s/files/1/0153/8863/products/Headphone-Zone-Final-Audio-E500-new_400x.jpg?v=1651153077",
+        name:"FINAL - E500",
+        productid:"er2",
+        type:"Earphones For Spatial Audio",
+        price:"Rs 1,599",
+      
+    },
+
+    {
+        btn:"PRICE DROP",
+        img:"https://cdn.shopify.com/s/files/1/0153/8863/products/Headphone-Zone-CCA-C10pro-Black-02_400x.jpg?v=1627994425",
+        name:"CCA - C10 PRO",
+        productid:"er3",
+        type:"In-Ears With 1DD + 4BA Driver",
+        price:"Rs 2,799",
+      
+    },
+
+    {
+        btn:"BEST SELLER",
+        img:"https://cdn.shopify.com/s/files/1/0153/8863/products/Headphone-Zone-SoundMAGIC-E11C-1160-1160-black_400x.jpg?v=1649233095",
+        name:"SOUNDMAGIC - E11C",
+        productid:"er4",
+        type:"In-Ears With 1 Dynamic Driver",
+        price:"Rs 2,499",
+      
+    },
+
+    {
+        btn:"BEST SELLER",
+        img:"https://cdn.shopify.com/s/files/1/0153/8863/products/Headphone-Zone-Final-Audio-E2000-Black-01_400x.jpg?v=1651151860",
+        name:"FINAL - E2000",
+        productid:"er5",
+        type:"In-Ears With 1 Dynamic Driver",
+        price:"Rs 2,799",
+       
+    },
+
+    {
+        btn:"BEST SELLER",
+        img:"https://cdn.shopify.com/s/files/1/0153/8863/products/kz-zs10-pro-headphone-zone-11557477056575_400x.jpg?v=1618074088",
+        name:"KZ - ZS10 PRO",
+        productid:"er6",
+        type:"In-Ears With 1 DD + 4 BA Drivers",
+        price:"Rs 4,399",
+    
+    },
+
+    {
+        btn:"OUR PICK",
+        img:"https://cdn.shopify.com/s/files/1/0153/8863/products/Silver-01_400x.jpg?v=1614671808",
+        name:"KZ - DQ6",
+        productid:"er7",
+        type:"In-Ears With 3 DD Drivers",
+        price:"Rs 2,299",
+       
+    },
+
+    {
+        btn:"BEST SELLER",
+        img:"https://cdn.shopify.com/s/files/1/0153/8863/products/Headphone-Zone-HiFiMAN-RE800-1160-1160-011_400x.jpg?v=1643803585",
+        name:"HIFIMAN - RE800",
+        productid:"er8",
+        type:"In-Ears With 9.2mm Nanotech Topology Dynamic Driver",
+        price:"Rs 11,999",
+      
+    },
+
+    {
+        btn:"BEST SELLER",
+        img:"https://cdn.shopify.com/s/files/1/0153/8863/products/Shure-Aonic215-Black-02_400x.jpg?v=1623754224",
+        name:"SHURE - AONIC 215",
+        productid:"er9",
+        type:"In-Ears With 1 Dynamic Driver",
+        price:"Rs 8,999",
+        
+    },
+
+    {
+        btn:"OUR PICK",
+        img:"https://cdn.shopify.com/s/files/1/0153/8863/products/397b9537bf78aea1a4a4acc4496a47fe_400x.jpg?v=1641184507",
+        name:"1 CUSTOM - JUNIOR",
+        productid:"er10",
+        type:"In-Ears With 1 DD + 2 BA Drivers",
+        price:"Rs 15,999",
+        
+    },
+    
+];
